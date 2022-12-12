82dc5aa829ddbbc6856064721831463f096f3cc8
Iratethisname10 <115908538+Iratethisname10@users.noreply.github.com>
2022-12-12 10:03:46 +0800

    Add files via upload

--- ui settings.lua
+++ ui settings.lua
@@ -1,3 +1,9 @@
+local repo = 'https://raw.githubusercontent.com/Iratethisname10/Iy-plus/main/repo/'
+
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'SaveManager.lua'))()
+
 Library:SetWatermarkVisibility(false)
 Library:SetWatermark('Infinte Yeild ++ | Made By Vo#1092')
 
