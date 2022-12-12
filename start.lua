4840a7958e7ef8be64e587d6f6f9ee105050f53b
Iratethisname10 <115908538+Iratethisname10@users.noreply.github.com>
2022-12-12 09:53:05 +0800

    Add files via upload

---
+++ start.lua
@@ -0,0 +1,5 @@
+local repo = 'https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/'
+
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
