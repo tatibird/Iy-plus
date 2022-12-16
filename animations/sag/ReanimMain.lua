5a8ca8c53090d4aabc746c71cdab1c4dffecb57f
Iratethisname10 <115908538+Iratethisname10@users.noreply.github.com>
2022-12-16 09:12:14 +0800

    Add files via upload

---
+++ animations/sag/ReanimMain.lua
@@ -0,0 +1,2 @@
+if not getgenv()._reanimate then getgenv()._reanimate = loadstring(game:HttpGet('https://raw.githubusercontent.com/shidemuri/coffeeware/main/reanim.lua'))() end
+return getgenv()._reanimate()
