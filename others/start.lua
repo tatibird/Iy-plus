57644b424e771bc3ae53c46d1579417d6eb718fe
Iratethisname10 <115908538+Iratethisname10@users.noreply.github.com>
2023-01-01 13:28:37 +0800

    update

---
+++ others/start.lua
@@ -0,0 +1,5 @@
+local repo = 'https://raw.githubusercontent.com/Iratethisname10/Iy-plus/main/repo/'
+
+local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet(repo .. 'ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet(repo .. 'SaveManager.lua'))()
--- start.lua
+++
@@ -1,5 +0,0 @@
-local repo = 'https://raw.githubusercontent.com/Iratethisname10/Iy-plus/main/repo/'
-
-local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
-local ThemeManager = loadstring(game:HttpGet(repo .. 'ThemeManager.lua'))()
-local SaveManager = loadstring(game:HttpGet(repo .. 'SaveManager.lua'))()
