9e0bf8aa78fac4b140064a849587dc53e2d72698
Iratethisname10 <115908538+Iratethisname10@users.noreply.github.com>
2023-01-02 08:20:48 +0800

    Add files via upload

---
+++ others/extras.lua
@@ -0,0 +1,6 @@
+wait(5)
+
+
+if game.Players.LocalPlayer.UserId == "1" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/Iy-plus/main/others/files/by.lua"))()
+end
