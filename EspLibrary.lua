5a5971c5c7ef3f613124f128b9c0b51360c00975
Iratethisname10 <115908538+Iratethisname10@users.noreply.github.com>
2023-01-15 15:46:32 +0800

    Add files via upload

--- EspLibrary.lua
+++ EspLibrary.lua
@@ -1,6 +1,6 @@
 --Settings--
 local ESP = {
-    Enabled = false,
+    Enabled = true,
     Boxes = false,
     BoxShift = CFrame.new(0,-1.5,0),
 	BoxSize = Vector3.new(4,6,0),
@@ -11,7 +11,7 @@ local ESP = {
     Thickness = 2,
     AttachShift = 1,
     TeamMates = true,
-    Players = false,
+    Players = true,
     
     Objects = setmetatable({}, {__mode="kv"}),
     Overrides = {
